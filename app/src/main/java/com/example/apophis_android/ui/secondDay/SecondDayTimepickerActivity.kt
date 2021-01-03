643fe7c799a125f0befbbd78fdfbd88b771c22ae
kimdahyee <diana6777@naver.com>
2021-01-04 04:56:46 +0900

    [Add] 2일차 timepicker 적용

--- app/src/main/java/com/example/apophis_android/ui/secondDay/SecondDayTimepickerActivity.kt
+++ app/src/main/java/com/example/apophis_android/ui/secondDay/SecondDayTimepickerActivity.kt
@@ -1,27 +1,24 @@
 package com.example.apophis_android.ui.secondDay
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TimePicker
 import com.example.apophis_android.R
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_second_day_timepicker.*
-import kotlinx.android.synthetic.main.dialog_timepicker.*
 
 class SecondDayTimepickerActivity : AppCompatActivity() {
 
-    private lateinit var bottomSheetDialog: BottomSheetDialog
-    private lateinit var timePicker: TimePicker
+    private lateinit var bottomSheetDialogFragment: SecondDayBottomSheetDialogFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_day_timepicker)
 
         btn_time_setting.setOnClickListener {
-            bottomSheetDialog = BottomSheetDialog(this)
-            bottomSheetDialog.setContentView(R.layout.dialog_timepicker)
-            bottomSheetDialog.show()
+            bottomSheetDialogFragment = SecondDayBottomSheetDialogFragment()
+            bottomSheetDialogFragment.show(supportFragmentManager, "tag")
         }
+
+        val intent = intent
+        tv_clock_time.text = intent.getStringExtra("time")
     }
+
 }
