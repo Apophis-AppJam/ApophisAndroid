e362ed1ccdfbe06ef6f1bcd2472501e1df545be2
wogus0333 <wogus0333@naver.com>
2021-01-03 23:29:32 +0900

    [Add] 1일차 카메라 layout 구현

---
+++ app/src/main/java/com/example/apophis_android/ui/firstDay/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.example.apophis_android.ui.firstDay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apophis_android.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
