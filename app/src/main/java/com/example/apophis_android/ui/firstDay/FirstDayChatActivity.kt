62b1ad081f072e4d1473c27305d4b16080f8c4c3
wogus0333 <wogus0333@naver.com>
2021-01-04 03:51:07 +0900

    [Add] 1일차 채팅 inflate 구현

--- app/src/main/java/com/example/apophis_android/ui/firstDay/FirstDayChatActivity.kt
+++ app/src/main/java/com/example/apophis_android/ui/firstDay/FirstDayChatActivity.kt
@@ -2,11 +2,80 @@ package com.example.apophis_android.ui.firstDay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.apophis_android.R
+import com.example.apophis_android.data.ChatData
+import kotlinx.android.synthetic.main.activity_first_day_chat.*
 
 class FirstDayChatActivity : AppCompatActivity() {
+
+    private lateinit var chatAdapter: FirstDayChatAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_day_chat)
+
+        chatAdapter = FirstDayChatAdapter(this)
+        rcv_first_chat.adapter = chatAdapter
+
+        val aponymous = mutableListOf("안녕! 미안 방금 정신없었지?",
+            "그냥, 방금 뉴스 봤어? 지구가 멸망한\n다길래 아무 번호로나 전화해봤어. 꼭\n한 번쯤 해보고 싶었거든.")
+
+//        val aponius = mutableListOf("넌 누구야")
+//        val aponis2 = mutableListOf("다음에 너가 골라줘야해")
+        val data = mutableListOf("넌 누구야?", "응.", "별로 믿기지 않아.")
+
+        aponymous.forEach {
+            //Log.d("아포니스가 무슨 말을 할지 보자", it)
+            val chatLeft = ChatData(mutableListOf(it), 0)
+            chatAdapter.addChat(chatLeft)
+        }
+
+//        aponius.forEach {
+//            Log.d("아포니어스는??", it)
+//            val chatRight = ChatData(mutableListOf(it), 1)
+//            chatAdapter.addChat(chatRight)
+//        }
+//
+//        aponis2.forEach {
+//            val chatLeft = ChatData(mutableListOf(it), 0)
+//            chatAdapter.addChat(chatLeft)
+//        }
+
+        val choice = ChatData(data, 2) //선택지
+        chatAdapter.addChat(choice)
+
+        chatAdapter.setOnChipItemClickListener(object : FirstDayChatAdapter.OnChipClickListener {
+            override fun onChipClick(data: String) {
+                // 아이템 클릭 이벤트를 MainActivity에서 처리.
+                et_first_chat_message.setText(data)
+            }
+        })
+
+        btn_first_send.setOnClickListener {
+            val userChoice = et_first_chat_message.text.toString()
+
+            val chatRight = ChatData(mutableListOf(userChoice), 1)
+            chatAdapter.addChat(chatRight)
+            chatAdapter.removeChat()
+            Toast.makeText(this,"hi",Toast.LENGTH_SHORT).show()
+            et_first_chat_message.setText("")
+            a()
+        }
     }
+    fun a(){
+        val aponymous1 = mutableListOf("음.. 내가 누군지는 일단 비밀이라고 해둘게.\n몇가지만 알려주자면", "우선 나는 동쪽 끝에 살아 동쪽이 어딘지 알\n아?")
+        val compass = mutableListOf("카메라")
+
+        aponymous1.forEach {
+            val chatLeft = ChatData(mutableListOf(it), 0)
+            chatAdapter.addChat(chatLeft)
+        }
+        val compass_btn = ChatData(compass, 2) //선택지
+        chatAdapter.addChat(compass_btn)
+    }
+//    private fun initRcv() {
+//        chatAdapter = FirstDayChatAdapter(this)
+//        rcv_first_chat.adapter = chatAdapter
+//    }
 }
---
+++ app/src/main/java/com/example/apophis_android/ui/firstDay/FirstDayChatAdapter.kt
@@ -0,0 +1,123 @@
+package com.example.apophis_android.ui.firstDay
+
+import android.content.Context
+import android.content.Intent
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apophis_android.R
+import com.example.apophis_android.data.ChatData
+import com.example.apophis_android.ui.ChipFactory
+import com.google.android.material.chip.ChipGroup
+import java.lang.IllegalArgumentException
+
+class FirstDayChatAdapter(private val context: Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val chatDataList: MutableList<ChatData> = mutableListOf()
+
+    override fun getItemViewType(position: Int): Int {
+        return when (chatDataList[position].tag) {
+            0 -> R.layout.item_chat_left
+            1 -> R.layout.item_chat_right
+            else -> R.layout.item_chat_choice
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            R.layout.item_chat_left -> {
+                val view = layoutInflater.inflate(R.layout.item_chat_left, parent, false)
+                ChatViewHolder(view)
+            }
+            R.layout.item_chat_right -> {
+                val view = layoutInflater.inflate(R.layout.item_chat_right, parent, false)
+                ChatViewHolder(view)
+            }
+            R.layout.item_chat_choice -> {
+                val view = layoutInflater.inflate(R.layout.item_chat_choice, parent, false)
+                ChoiceViewHolder(view, layoutInflater)
+            }
+            else ->
+                throw IllegalArgumentException("ViewType [$viewType] is unexpected")
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ChatViewHolder) {
+            holder.bind(chatDataList[position].content)
+            holder.itemView.animation = AnimationUtils.loadAnimation(context, R.anim.translate_up)
+        }
+        if (holder is ChoiceViewHolder) {
+            holder.bind(chatDataList[position].content)
+            holder.itemView.animation = AnimationUtils.loadAnimation(context, R.anim.translate_up)
+        }
+    }
+
+    inner class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val content = itemView.findViewById<TextView>(R.id.chat_content)
+        fun bind(chatDataList: MutableList<String>) {
+            chatDataList.forEach {
+                content.text = it
+            }
+        }
+    }
+
+    inner class ChoiceViewHolder(itemView: View, private val inflater: LayoutInflater) : RecyclerView.ViewHolder(itemView) {
+
+        private var chipGroup: ChipGroup = itemView.findViewById(R.id.chipgroup_choice)
+
+        fun bind(chipItem: MutableList<String>) {
+            chipGroup.removeAllViews()
+            chipItem.forEach {
+                val chip = ChipFactory.newInstance(inflater)
+                chip.text = it
+                chip.isClickable = true
+                chipGroup.addView(chip)
+
+                chip.setOnClickListener {
+                    if (chip.text.toString() == "나침반") {
+                        val intent = Intent(context, CompassActivity::class.java)
+                        context.startActivity(intent)
+                    } else if(chip.text.toString() == "카메라"){
+                        val intent = Intent(context, CameraActivity::class.java)
+                        context.startActivity(intent)
+                    } else {
+                        chipClickListener.onChipClick(chip.text.toString())
+                    }
+                }
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return chatDataList.size
+    }
+
+    fun addChat(chatItem: ChatData) {
+        chatDataList.add(chatItem)
+        notifyItemInserted(chatDataList.size)
+    }
+
+    fun removeChat() {
+        chatDataList.removeAt(chatDataList.size-2)
+        notifyItemRemoved(chatDataList.size-1)
+    }
+
+    /* chip click listener */
+    interface OnChipClickListener {
+        fun onChipClick(data: String)
+    }
+
+    private lateinit var chipClickListener: OnChipClickListener
+
+    fun setOnChipItemClickListener(listener: OnChipClickListener) {
+        this.chipClickListener = listener
+    }
+}
