9bec896790ab39d25280b3be406cb7f300529c19
kimdahyee <diana6777@naver.com>
2021-01-04 14:10:41 +0900

    [Add] 2일차 find me Layout 작성

---
+++ app/src/main/java/com/example/apophis_android/ui/secondDay/SecondDayFindDarkMeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.apophis_android.ui.secondDay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apophis_android.R
+
+class SecondDayFindDarkMeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_day_find_dark_me)
+    }
+}
