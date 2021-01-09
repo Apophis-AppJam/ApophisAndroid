039910a4e60a3a05df0715dd9c8b1aab7eb8285c
Cho Sungrim <srcho730@gmail.com>
2021-01-09 20:42:27 +0900

    [Add] 자동 로그인 추가

--- app/src/main/java/com/example/apophis_android/MainActivity.kt
+++ app/src/main/java/com/example/apophis_android/MainActivity.kt
@@ -1,39 +1,12 @@
 package com.example.apophis_android
 
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        getHashKey()
-    }
-
-    private fun getHashKey() {
-        var packageInfo: PackageInfo? = null
-        try {
-            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-        } catch (e: PackageManager.NameNotFoundException) {
-            e.printStackTrace()
-        }
-        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
-        for (signature in packageInfo!!.signatures) {
-            try {
-                val md: MessageDigest = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
-            } catch (e: NoSuchAlgorithmException) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
-            }
-        }
     }
 }
--- app/src/main/java/com/example/apophis_android/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/apophis_android/ui/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.apophis_android.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.apophis_android.MainActivity
 import com.example.apophis_android.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -12,11 +13,26 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.kakao.sdk.auth.LoginClient
-import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.auth.AuthType
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+import com.kakao.network.ErrorResult
+import com.kakao.usermgmt.ApiErrorCode
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.MeV2ResponseCallback
+import com.kakao.usermgmt.response.MeV2Response
+import com.kakao.util.exception.KakaoException
 import kotlinx.android.synthetic.main.activity_login.*
 
+/**
+ * Created By chosungrim
+ * on 01월 09일, 2020
+ */
+
 class LoginActivity : AppCompatActivity() {
+
+    //카카오 로그인 콜백 선언
+    private var sessionCallback: SessionCallback? = null
 
     // Google Sign-In Methods
     var googleSignInClient : GoogleSignInClient? = null
@@ -30,27 +46,11 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // 카카오 로그인 공통 callback 구성
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                //Login Fail
-            } else if (token != null) {
-                //Login Success
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
-        }
+        sessionCallback = SessionCallback()
+        Session.getCurrentSession().addCallback(sessionCallback)
+        Session.getCurrentSession().checkAndImplicitOpen()
 
-        login_kakao_btn.setOnClickListener {
-            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-            LoginClient.instance.run {
-                if (isKakaoTalkLoginAvailable(this@LoginActivity)) {
-                    loginWithKakaoTalk(this@LoginActivity, callback = callback)
-                } else {
-                    loginWithKakaoAccount(this@LoginActivity, callback = callback)
-                }
-            }
-        }
+        login_kakao_btn.setOnClickListener { Session.getCurrentSession().open(AuthType.KAKAO_LOGIN_ALL, this@LoginActivity) }
 
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -86,10 +86,13 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        MoveNextPage() // Automatic Login
+        MoveNextPage() // Google Automatic Login
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return
+        }
         super.onActivityResult(requestCode, resultCode, data)
 
         // Google Sign-In Methods
@@ -104,6 +107,42 @@ class LoginActivity : AppCompatActivity() {
             }
         } else {
 
+        }
+    }
+
+    override fun onDestroy() {
+        //카카오 로그인 콜백 제거
+        super.onDestroy()
+        Session.getCurrentSession().removeCallback(sessionCallback)
+    }
+
+    private inner class SessionCallback : ISessionCallback {
+        override fun onSessionOpened() { //세션이 성공적으로 열린 경우
+            UserManagement.getInstance().me(object : MeV2ResponseCallback() {
+                //유저 정보를 가져온다.
+                override fun onFailure(errorResult: ErrorResult) { //유저 정보를 가져오는 데 실패한 경우
+                    val result: Int = errorResult.getErrorCode() //오류 코드를 받아온다.
+                    if (result == ApiErrorCode.CLIENT_ERROR_CODE) { //클라이언트 에러인 경우: 네트워크 오류
+                        Toast.makeText(applicationContext, "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else { //클라이언트 에러가 아닌 경우: 기타 오류
+                        Toast.makeText(applicationContext, "로그인 도중 오류가 발생했습니다: " + errorResult.getErrorMessage(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onSuccess(result: MeV2Response?) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                override fun onSessionClosed(errorResult: ErrorResult) { //세션이 도중에 닫힌 경우
+                    Toast.makeText(applicationContext, "세션이 닫혔습니다. 다시 시도해 주세요: " + errorResult.getErrorMessage(), Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        override fun onSessionOpenFailed(e: KakaoException) { //세션을 여는 도중 오류가 발생한 경우 -> Toast 메세지를 띄움.
+            Toast.makeText(applicationContext, "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: " + e.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
