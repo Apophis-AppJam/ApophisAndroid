b8f84ed5352406cc4b061341bf6eea880e8c1989
kimdahyee <diana6777@naver.com>
2021-01-15 18:15:08 +0900

    [Chore] stash 하기 싫어서 하는 커밋

---
+++ app/src/main/java/com/example/apophis_android/ui/secondDay/SecondDayChatEndingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.apophis_android.ui.secondDay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apophis_android.R
+
+class SecondDayChatEndingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_day_chat_ending)
+    }
+}
